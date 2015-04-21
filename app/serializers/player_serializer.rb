11d03014fb331f7287370c1056e1ab07bc8c6690
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-04-21 14:28:02 -0400

    Remove unused

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -5,15 +5,10 @@ class PlayerSerializer < ActiveModel::Serializer
   has_many :gameplayers
   has_many :games
   # has_many :scores
-  #belongs_to :game
-  #has_many :games, through: :scores
-
-  # def score_id
-  #   object.score.id
-  # end
+  # belongs_to :game
+  # has_many :games, through: :scores
 
   def scores
-    #Jason:
     # object.gameplayers is an array, we have to iterate over that array,
     # for each object.gameplayers, we have to query for the scores of that gameplayer, and return the score's data.
 
@@ -22,14 +17,11 @@ class PlayerSerializer < ActiveModel::Serializer
 
 
       gameplayer.scores.each { |score|
-      # debugger
         # gp = Gameplayer.where(id: score.gameplayer_id)
         game_id = Gameplayer.where(id: score.gameplayer_id).first.game_id
         o << { "id" => score.id, "points" => score.points, "game_id" => game_id }
       }
     }
 
-    #Tom:
-    # self.gameplayers.map(&:scores).flatten
   end
 end
