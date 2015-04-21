231114366c55f7eb343624674d650c7b36422116
Nicole Pupillo <nicole.pupillo@gmail.com>
2015-04-21 14:23:49 -0400

    Add belongs to player

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,14 +1,15 @@
 class Score < ActiveRecord::Base
   belongs_to :gameplayer
   belongs_to :game
+  belongs_to :player
 
 
-  def player
-    self.gameplayer.player
-  end
+  # def player
+  #   self.gameplayer.player
+  # end
 
-  def game
-    self.gameplayer.game
-  end
+  # def game
+  #   self.gameplayer.game
+  # end
 
 end
